90dea7ab5603f7c0521aee230658f8da583e28e6
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2019-12-01 22:19:46 -0800

    day2 pt1 passing tests

--- spec/advent_of_code_2019/intcode_spec.clj
+++ spec/advent_of_code_2019/intcode_spec.clj
@@ -4,6 +4,15 @@
 
 (describe "intcode"
           (it "works for test input"
-              (should= 2 (intcode))))
+              (should= [99 8 7 6] (execute [99 8 7 6]))
+              (should= [2,0,0,0,99] (execute [1,0,0,0,99]))
+              (should= [2,3,0,6,99] (execute [2,3,0,3,99]))
+              (should= [2,4,4,5,99,9801] (execute [2,4,4,5,99,0]))
+              (should= [30,1,1,4,2,5,6,0,99] (execute [1,1,1,4,99,5,6,0,99]))
+              (should= [3500,9,10,70,2,3,11,0,99,30,40,50]
+                       (execute [1,9,10,3,2,3,11,0,99,30,40,50]))
+              ))
+
+              
 
 (run-specs)
--- src/advent_of_code_2019/intcode.clj
+++ src/advent_of_code_2019/intcode.clj
@@ -1,4 +1,29 @@
 (ns advent-of-code-2019.intcode
   (:gen-class))
 
-(defn intcode [] 2)
+(defn get-instruction [program instruction-index]
+  (vec ((vec (partition 4 4 [0 0 0] program)) instruction-index)))
+
+(defn first-input [instruction program]
+  (program (instruction 1)))
+
+(defn second-input [instruction program]
+  (program (instruction 2)))
+
+(defn execute-add [instruction program]
+  (let [add-result (+ (first-input instruction program) (second-input instruction program))]
+    (assoc program (instruction 3) add-result)))
+
+(defn execute-mult [instruction program]
+  (let [mult-result (* (first-input instruction program) (second-input instruction program))]
+    (assoc program (instruction 3) mult-result)))
+
+(defn execute [program]
+  (loop [instruction-index 0
+         curr-program program]
+    (let [instruction (get-instruction curr-program instruction-index)
+          opcode (first instruction)]
+      (case opcode
+        99 curr-program
+        1 (recur (inc instruction-index) (execute-add instruction curr-program))
+        2 (recur (inc instruction-index) (execute-mult instruction curr-program))))))
