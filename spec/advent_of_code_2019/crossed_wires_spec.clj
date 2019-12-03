e767cf19223a0a7b5a5b8b633c0781f71ea44c1f
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2019-12-03 00:35:42 -0800

    day3pt passes unit tests

--- spec/advent_of_code_2019/crossed_wires_spec.clj
+++ spec/advent_of_code_2019/crossed_wires_spec.clj
@@ -14,5 +14,29 @@
                         '(2 1) '(2 2)] (wire-coords ['("R" 2) '("U" 2)]))
               ))
 
+(describe "parse"
+          (it "works for test input"
+              (should= ['("R" 1)] (parse "R1"))
+              (should= ['("R" 1) '("U" 11) '("L" 222) '("D" 9)] (parse "R1,U11,L222,D9"))))
+
+(describe "closest-overlap-coords"
+          (it "works for test input"
+              (should= [1 0] (closest-overlap-coords [[1 0]] [[1 0]]))
+              (should= [10 0] (closest-overlap-coords [[0 0] [1 0] [10 0] [100 0]] 
+                                                     [[0 0] [2 0] [10 0] [100 0]]))
+              ))
+
+(describe "closet-overlap"
+          (it "works for test input"
+              (should= [1 0] (closet-overlap "R2" "R2"))
+              (should= [-1 2] (closet-overlap "R2,U2,L10" "L1,U10"))))
+
+(describe "closet-overlap-dist"
+          (it "works for test input"
+              (should= 1 (closet-overlap-dist "R2" "R2"))
+              (should= 3 (closet-overlap-dist "R2,U2,L10" "L1,U10"))
+              (should= 159 (closet-overlap-dist "R75,D30,R83,U83,L12,D49,R71,U7,L72" "U62,R66,U55,R34,D71,R55,D58,R83"))
+              (should= 135 (closet-overlap-dist "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"))
+              ))
 
 (run-specs)
--- src/advent_of_code_2019/crossed_wires.clj
+++ src/advent_of_code_2019/crossed_wires.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code-2019.crossed-wires
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
 (defn right-coord [start-coord index]
   [ (+ (first start-coord) 1 index) (second start-coord)])
@@ -26,3 +27,26 @@
     (if (empty? directions)
         acc
         (recur (rest directions) (concat acc (process-direction (first directions) (last acc)))))))
+
+(defn parse-direction [direction-str]
+  [(subs direction-str 0 1) (Integer/parseInt (subs direction-str 1))])
+
+(defn parse [wire-directions-str]
+  (map parse-direction (str/split wire-directions-str #",")))
+
+(defn manhattan-distance [coord]
+  (+ (Math/abs (first coord)) (Math/abs (second coord))))
+
+(defn not-origin [coord]
+  (not= '(0 0) coord))
+
+(defn closest-overlap-coords [coordsA coordsB]
+  (let [overlaps (clojure.set/intersection (set coordsA) (set coordsB))]
+    (first (sort-by manhattan-distance (filter not-origin overlaps)))))
+
+(defn closet-overlap [wire-directions-str1 wire-directions-str2]
+  (closest-overlap-coords (wire-coords (parse wire-directions-str1))
+                          (wire-coords (parse wire-directions-str2))))
+
+(defn closet-overlap-dist [wire-directions-str1 wire-directions-str2]
+  (manhattan-distance (closet-overlap wire-directions-str1  wire-directions-str2)))
