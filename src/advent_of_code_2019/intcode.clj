491c029e4c823b97700ee1de0186ca6855699ba5
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2019-12-07 09:58:24 -0800

    extract get-param

--- src/advent_of_code_2019/intcode.clj
+++ src/advent_of_code_2019/intcode.clj
@@ -21,25 +21,27 @@
 (defn next-instruction-address [instruction-address opcode]
   (+ instruction-address (opcode-ins-lengh opcode)))
 
-(defn first-input [instruction program]
-  (program (instruction 1)))
+(defn get-param [num instruction program]
+  (let [param-addr (get instruction num)]
+    (if-not (= param-addr nil) (program param-addr))))
 
 (defn second-input [instruction program]
-  (program (instruction 2)))
+  (get-param 2 instruction program))
 
-(defn execute-add [instruction program]
-  (let [add-result (+ (first-input instruction program) (second-input instruction program))]
+(defn execute-add [first-param instruction program]
+  (let [add-result (+ first-param (second-input instruction program))]
     (assoc program (instruction 3) add-result)))
 
-(defn execute-mult [instruction program]
-  (let [mult-result (* (first-input instruction program) (second-input instruction program))]
+(defn execute-mult [first-param instruction program]
+  (let [mult-result (* first-param (second-input instruction program))]
     (assoc program (instruction 3) mult-result)))
 
 (defn execute-instruction [instruction program]
-  (let [opcode (first instruction)]
+  (let [opcode (get instruction 0)
+        first-param (get-param 1 instruction program)]
     (case opcode
-      1 (execute-add instruction program)
-      2 (execute-mult instruction program)
+      1 (execute-add first-param instruction program)
+      2 (execute-mult first-param instruction program)
       3 program
       4 program)))
 
