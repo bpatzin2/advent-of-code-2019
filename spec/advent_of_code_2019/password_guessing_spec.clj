6b5dd1127c5ed76ea0a42fe90d3ee3c78cd7e2bc
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2019-12-08 15:23:19 -0800

    day4 pt1 passing tests, pt2 failing

--- spec/advent_of_code_2019/password_guessing_spec.clj
+++ spec/advent_of_code_2019/password_guessing_spec.clj
@@ -2,10 +2,38 @@
   (:require [speclj.core :refer :all]
             [advent-of-code-2019.password-guessing :refer :all]))
 
+(describe "never-decrease?"
+          (it "works for test input"
+              (should= true (never-decrease? 111))
+              (should= false (never-decrease? 211))))
+
+(describe "same-adj-digs?"
+          (it "works for test input"
+              (should= true (same-adj-digs? 112))
+              (should= false (same-adj-digs? 121))))
+
+(describe "meets-criteria"
+          (it "works for test input"
+              (should= true (meets-orig-criteria? 111))
+              (should= false (meets-orig-criteria? 121))
+              (should= true (meets-orig-criteria? 111111))
+              (should= false (meets-orig-criteria? 223450))
+              (should= false (meets-orig-criteria? 123789))))
+
 (describe "options"
           (it "works for test input"
-              (should= 2 (options 0 1))
+              (should= '(111 112 113 114 115 116 117 118 119) 
+                       (orig-options 100 120))
               ))
+
+(describe "meets-updated-criteria"
+          (it "works for test input"
+              (should= true (meets-updated-criteria? 112))
+              (should= false (meets-updated-criteria? 111))
+              (should= false (meets-updated-criteria? 121))
+              (should= true (meets-updated-criteria? 112233))
+              (should= false (meets-updated-criteria? 123444))
+              (should= true (meets-updated-criteria? 111122))))
 
 
 (run-specs)
--- src/advent_of_code_2019/password_guessing.clj
+++ src/advent_of_code_2019/password_guessing.clj
@@ -1,4 +1,29 @@
 (ns advent-of-code-2019.password-guessing
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
-(defn options [range-start range-end] 2)
+(defn char-vec [num]
+  (str/split (str num) #""))
+
+(defn dig-vec [num]
+  (map #(Integer/parseInt %) (char-vec num)))
+
+(defn never-decrease? [num]
+  (not= false (reduce #(if (> %1 %2) (reduced false) %2) (dig-vec num))))
+
+(defn same-adj-digs? [num]
+  (= true (reduce #(or (= true %1) (if (= %1 %2) true %2)) (char-vec num))))
+
+(defn meets-orig-criteria? [num]
+  (and (same-adj-digs? num)
+       (never-decrease? num)))
+
+(defn meets-updated-criteria? [num]
+  (and (same-adj-digs? num)
+       (never-decrease? num)))
+
+(defn orig-options [range-start range-end] 
+  (filter meets-orig-criteria? (range range-start range-end)))
+
+(defn updated-options [range-start range-end]
+  (filter meets-updated-criteria? (range range-start range-end)))
