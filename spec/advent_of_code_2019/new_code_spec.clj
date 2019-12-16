0ae132cd8669f53d14c586a01c555b88ae0ba71a
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2019-12-15 23:10:36 -0800

    day16pt1 passing tests

--- spec/advent_of_code_2019/new_code_spec.clj
+++ spec/advent_of_code_2019/new_code_spec.clj
@@ -2,6 +2,22 @@
   (:require [speclj.core :refer :all]
             [advent-of-code-2019.new-code :refer :all]))
 
-(describe "run"
+(describe "gen-pattern"
           (it "works for test input"
-              (should= 2 (run "test"))))
+              (should= [1 0 -1 0 1 0 -1 0] (gen-pattern 0 8))
+              (should= [0 1 1 0 0 -1 -1 0] (gen-pattern 1 8))))
+
+(describe "run-fft"
+          (it "works for test input"
+              (should= [4 8 2 2 6 1 5 8] 
+                       (run-fft [1 2 3 4 5 6 7 8] 1))
+              (should= [3 4 0 4 0 4 3 8]
+                       (run-fft [1 2 3 4 5 6 7 8] 2))
+              ))
+
+(describe "run-fft-str"
+          (it "works for test input"
+              (should= "24176176"
+                       (run-fft-str "80871224585914546619083218645595" 
+                                    100 8))
+              ))
--- src/advent_of_code_2019/new_code.clj
+++ src/advent_of_code_2019/new_code.clj
@@ -1,6 +1,44 @@
 (ns advent-of-code-2019.new-code
   (:gen-class)
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str-lib]))
 
-(defn run [num]
-  2)
+(defn apply-pattern [nums, pattern]
+  (->> pattern
+       (interleave nums)
+       (partition 2)
+       (map #(apply * %))
+       (reduce +)
+       (#(rem % 10))
+       (Math/abs)))
+
+(defn pattern-val [pos-in-list i]
+  (let [base-pattern [0 1 0 -1]
+        index (rem (quot i (inc pos-in-list)) 4)]
+    (base-pattern index)))
+
+(defn gen-pattern [pos-in-list, len]
+  (loop [res []
+         i 0]
+    (if
+     (= len (count res))
+      res
+      (let [val (pattern-val pos-in-list i)]
+        (recur
+         (if (>= i 1) (conj res val) res)
+         (inc i))))))
+
+(defn output-digit [pos-in-list num nums]
+  (apply-pattern nums (gen-pattern pos-in-list (count nums))))
+
+(defn apply-phase [nums _]
+  (flatten (map-indexed #(output-digit %1 %2 nums) nums)))
+
+(defn run-fft [nums phases]
+  (reduce apply-phase nums (range 0 phases)))
+
+(defn str-as-int-vec [str]
+  (vec (map #(Integer/parseInt %) (str-lib/split str #""))))
+
+(defn run-fft-str [str phases out-count] 
+ (let [res (vec (run-fft (str-as-int-vec str) phases))]
+   (apply clojure.core/str (subvec res 0 out-count))))
