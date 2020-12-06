095a0aa8bbecca7180ef746bfbbe15ed35d07ad1
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2020-12-06 19:06:25 +0100

    total-energy passing tests to test input

--- spec/advent_of_code_2019/n_body_problem_spec.clj
+++ spec/advent_of_code_2019/n_body_problem_spec.clj
@@ -3,47 +3,47 @@
             [advent-of-code-2019.n-body-problem :refer :all]
             [clojure.string :as str]))
 
-;(def test-moons
-;  [{:name "m1" :v [1 7 0] :pos [4 7 0]},
-;   {:name "m2" :v [1 2 3] :pos [4 5 6]},
-;   {:name "m3" :v [1 2 3] :pos [4 6 8]}])
-;
-;(describe
-; "velocity-changes"
-; (it "works for test input"
-;     (let [updated-moons (velocity-changes test-moons)]
-;       (should= '(0 -2 2)  (:dv (first updated-moons)))
-;       (should= '(0 2 0)  (:dv (second updated-moons)))
-;       (should= '(0 0 -2)  (:dv (nth updated-moons 2)))))
-; )
-;
-;(def test-data
-;  (str/trim "
-;  <x=-1, y=0, z=2>
-;  <x=2, y=-10, z=-7>
-;  <x=4, y=-8, z=8>
-;  <x=30, y=50, z=-10>
-;  "))
-;
-;(def test-lines (str/split test-data #"\n"))
-;
-;(describe
-; "create-moons"
-; (it "works for test input"
-;     (let [actual-moons (map-indexed create-moon test-lines)]
-;       (should= '(-1 0 2) (:pos (nth actual-moons 0)))
-;       (should= '(2 -10 -7) (:pos (nth actual-moons 1)))
-;       (should= '(4 -8 8) (:pos (nth actual-moons 2)))
-;       (should= '(30 50 -10) (:pos (nth actual-moons 3))))))
-;
-;(describe
-; "apply-vel-update"
-; (it "works for test input"
-;     (let [moon (create-moon 0 '(1 2 3) '(1 1 1))
-;           moon-w-dv (assoc moon :dv '(0 -1 2)) ]
-;       (should= '(1 0 3)  (new-velocity moon-w-dv))
-;       (should= '(1 0 3)  (:vel (apply-vel-update moon-w-dv)))
-;       (should= '(2 2 6)  (:pos (apply-vel-update moon-w-dv))))))
+(def test-moons
+  [{:name "m1" :v [1 7 0] :pos [4 7 0]},
+   {:name "m2" :v [1 2 3] :pos [4 5 6]},
+   {:name "m3" :v [1 2 3] :pos [4 6 8]}])
+
+(describe
+ "velocity-changes"
+ (it "works for test input"
+     (let [updated-moons (moons-w-velocity-changes test-moons)]
+       (should= '(0 -2 2)  (:dv (first updated-moons)))
+       (should= '(0 2 0)  (:dv (second updated-moons)))
+       (should= '(0 0 -2)  (:dv (nth updated-moons 2)))))
+ )
+
+(def test-data
+  (str/trim "
+  <x=-1, y=0, z=2>
+  <x=2, y=-10, z=-7>
+  <x=4, y=-8, z=8>
+  <x=30, y=50, z=-10>
+  "))
+
+(def test-lines (str/split test-data #"\n"))
+
+(describe
+ "create-moons"
+ (it "works for test input"
+     (let [actual-moons (map-indexed create-moon test-lines)]
+       (should= '(-1 0 2) (:pos (nth actual-moons 0)))
+       (should= '(2 -10 -7) (:pos (nth actual-moons 1)))
+       (should= '(4 -8 8) (:pos (nth actual-moons 2)))
+       (should= '(30 50 -10) (:pos (nth actual-moons 3))))))
+
+(describe
+ "apply-vel-update"
+ (it "works for test input"
+     (let [moon (create-moon 0 '(1 2 3) '(1 1 1))
+           moon-w-dv (assoc moon :dv '(0 -1 2)) ]
+       (should= '(1 0 3)  (new-velocity moon-w-dv))
+       (should= '(1 0 3)  (:vel (apply-vel-update moon-w-dv)))
+       (should= '(2 2 6)  (:pos (apply-vel-update moon-w-dv))))))
 
 (defn assert-moon [expected-pos expected-vel actual-moon]
   (should= expected-pos (:pos actual-moon))
@@ -99,4 +99,23 @@
        (assert-moon '(2 1 -3) '(-3 -2 1) (nth moons10 0))
        (should= 179 total-energy)
        )))
+
+(def test-data2
+  (str/trim "
+    <x=-8, y=-10, z=0>
+    <x=5, y=5, z=10>
+    <x=2, y=-7, z=3>
+    <x=9, y=-8, z=-3>
+  "))
+
+(def test-lines2 (str/split test-data2 #"\n"))
+
+(describe
+ "test input"
+ (it "works for test input"
+     (let [actual-moons (map-indexed create-moon test-lines2)
+           moons100 (apply-time actual-moons 100)
+           total-energy (total-energy actual-moons 100)]
+       (assert-moon '(8 -12 -9) '(-7 3 0) (nth moons100 0))
+       (should= 1940 total-energy))))
 
