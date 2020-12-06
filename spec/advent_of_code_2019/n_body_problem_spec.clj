cbb3e0c6d07770cc21caabd4faaf05c4be30c435
Brian Patzin <bpatzin2@gmail.com>
2020-12-06 19:31:15 +0100

    Moons (#2)
    * simple test passing
    
    * some cleanup of n-body-problem
    
    * create-moon and input parsing
    
    * apply-vel-update working
    
    * apply-time works 1 step
    
    * apply-time works
    
    * total-energy passing tests
    
    * total-energy passing tests to test input
    
    * day 12 works
    
    * cleanup
    
    * cleanup
    
    Co-authored-by: Brian Patzin <bpatzin@Brians-MacBook-Air.local>

---
+++ spec/advent_of_code_2019/n_body_problem_spec.clj
@@ -0,0 +1,121 @@
+(ns advent-of-code-2019.n-body-problem
+  (:require [speclj.core :refer :all]
+            [advent-of-code-2019.n-body-problem :refer :all]
+            [clojure.string :as str]))
+
+(def test-moons
+  [{:name "m1" :v [1 7 0] :pos [4 7 0]},
+   {:name "m2" :v [1 2 3] :pos [4 5 6]},
+   {:name "m3" :v [1 2 3] :pos [4 6 8]}])
+
+(describe
+ "velocity-changes"
+ (it "works for test input"
+     (let [updated-moons (apply-gravity test-moons)]
+       (should= '(0 -2 2)  (:dv (first updated-moons)))
+       (should= '(0 2 0)  (:dv (second updated-moons)))
+       (should= '(0 0 -2)  (:dv (nth updated-moons 2)))))
+ )
+
+(def test-data
+  (str/trim "
+  <x=-1, y=0, z=2>
+  <x=2, y=-10, z=-7>
+  <x=4, y=-8, z=8>
+  <x=30, y=50, z=-10>
+  "))
+
+(def test-lines (str/split test-data #"\n"))
+
+(describe
+ "create-moons"
+ (it "works for test input"
+     (let [actual-moons (create-moons test-lines)]
+       (should= '(-1 0 2) (:pos (nth actual-moons 0)))
+       (should= '(2 -10 -7) (:pos (nth actual-moons 1)))
+       (should= '(4 -8 8) (:pos (nth actual-moons 2)))
+       (should= '(30 50 -10) (:pos (nth actual-moons 3))))))
+
+(describe
+ "apply-velocity"
+ (it "works for test input"
+     (let [moon (create-moon 0 '(1 2 3) '(1 1 1))
+           moon-w-dv (assoc moon :dv '(0 -1 2)) ]
+       (should= '(1 0 3)  (new-velocity moon-w-dv))
+       (should= '(1 0 3)  (:vel (apply-velocity moon-w-dv)))
+       (should= '(2 2 6)  (:pos (apply-velocity moon-w-dv))))))
+
+(defn assert-moon [expected-pos expected-vel actual-moon]
+  (should= expected-pos (:pos actual-moon))
+  (should= expected-vel (:vel actual-moon)))
+
+;<x=-1, y=0, z=2>
+;<x=2, y=-10, z=-7>
+;<x=4, y=-8, z=8>
+;<x=3, y=5, z=-1>
+(def moons-for-updating
+  [(create-moon "m1" [-1 0 2] [0 0 0]),
+   (create-moon "m2" [2 -10 -7] [0 0 0]),
+   (create-moon "m3" [4 -8 8] [0 0 0])
+   (create-moon "m4" [3 5 -1] [0 0 0])])
+
+;pos=<x= 2, y=-1, z= 1>, vel=<x= 3, y=-1, z=-1>
+;pos=<x= 3, y=-7, z=-4>, vel=<x= 1, y= 3, z= 3>
+;pos=<x= 1, y=-7, z= 5>, vel=<x=-3, y= 1, z=-3>
+;pos=<x= 2, y= 2, z= 0>, vel=<x=-1, y=-3, z= 1>
+(describe
+ "apply-time"
+ (it "works for test input"
+     (let [moons (apply-time moons-for-updating) ]
+       (assert-moon '(2 -1 1) '(3 -1 -1) (nth moons 0))
+       (assert-moon '(3 -7 -4) '(1 3 3) (nth moons 1))
+       (assert-moon '(1 -7 5) '(-3 1 -3) (nth moons 2))
+       (assert-moon '(2 2 0) '(-1 -3 1) (nth moons 3)))))
+
+;After 0 steps:
+;pos=<x=-1, y=  0, z= 2>, vel=<x= 0, y= 0, z= 0>
+;After 1 step:
+;pos=<x= 2, y=-1, z= 1>, vel=<x= 3, y=-1, z=-1>
+;After 2 steps:
+;pos=<x= 5, y=-3, z=-1>, vel=<x= 3, y=-2, z=-2>
+;After 3 steps:
+;pos=<x= 5, y=-6, z=-1>, vel=<x= 0, y=-3, z= 0>
+(describe
+ "apply-time with steps"
+ (it "works for test input"
+     (let [moons1 (apply-time moons-for-updating 1)
+           moons2 (apply-time moons-for-updating 2)
+           moons3 (apply-time moons-for-updating 3)]
+       (assert-moon '(2 -1 1) '(3 -1 -1) (nth moons1 0))
+       (assert-moon '(5 -3 -1) '(3 -2 -2) (nth moons2 0))
+       (assert-moon '(5 -6 -1) '(0 -3 0) (nth moons3 0)))))
+
+(describe
+ "energy"
+ (it "works for test input"
+     (let [moons10 (apply-time moons-for-updating 10)
+           total-energy (total-energy moons-for-updating 10)
+           ]
+       (assert-moon '(2 1 -3) '(-3 -2 1) (nth moons10 0))
+       (should= 179 total-energy)
+       )))
+
+(def test-data2
+  (str/trim "
+    <x=-8, y=-10, z=0>
+    <x=5, y=5, z=10>
+    <x=2, y=-7, z=3>
+    <x=9, y=-8, z=-3>
+  "))
+
+(def test-lines2 (str/split test-data2 #"\n"))
+
+(describe
+ "test input"
+ (it "works for test input"
+     (let [actual-moons (create-moons test-lines2)
+           moons100 (apply-time actual-moons 100)
+           total-energy (total-energy actual-moons 100)]
+       (assert-moon '(8 -12 -9) '(-7 3 0) (nth moons100 0))
+       (should= 1940 total-energy))))
+
---
+++ src/advent_of_code_2019/n_body_problem.clj
@@ -0,0 +1,104 @@
+(ns advent-of-code-2019.n-body-problem
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
+
+(def moons
+  [{:id "m1" :vel [1 7 0] :pos [4 7 0]},
+   {:id "m2" :vel [1 2 3] :pos [4 5 6]},
+   {:id "m3" :vel [1 2 3] :pos [4 5 6]}])
+
+;https://stackoverflow.com/questions/15858365/how-to-use-merge-with-to-create-a-list-for-duplicate-keys-in-clojure
+;Needed this to group a map by keys and collect grouped values in a list
+(defn my-merge [f & mlist]
+  (into {}
+        (map (fn [[k v]] [k (apply f (map val v))])
+             (group-by key (apply concat mlist)))))
+
+(defn pairs [items]
+  (combo/combinations items 2))
+
+(defn velocity-change-ps [p1 p2]
+  (cond
+    (> p1 p2) '(-1 1)
+    (< p1 p2) '(1 -1)
+    :else '(0 0)))
+
+(defn velocity-change
+  ([moon-pair]
+   (velocity-change (first moon-pair) (second moon-pair)))
+  ([m1 m2]
+    (let [p1 (:pos m1)
+          p2 (:pos m2)
+          changes (map velocity-change-ps p1 p2)]
+      {m1 (map first changes)
+       m2 (map second changes)})))
+
+(defn vel-changes-per-moon [moons]
+  (let[all-changes (map velocity-change (pairs moons))]
+    (apply my-merge list all-changes)))
+
+(defn assoc-dv-in-moon [moon-changes]
+  (let[moon (first moon-changes)
+       vel-changes (second moon-changes)]
+    (assoc moon :dv (reduce #(map + %1 %2) vel-changes))))
+
+(defn moon-pos-from-string [str]
+  (map #(Integer/parseInt %) (re-seq #"-?\d+" str)))
+
+(defn new-pos [curr-pos vel]
+  (map + curr-pos vel))
+
+(defn new-velocity
+  ([moon]
+   (new-velocity (:vel moon) (:dv moon)))
+  ([curr-vel dv]
+   (map + curr-vel dv)))
+
+(defn apply-gravity [moons]
+  (let[changes-per-moon (vel-changes-per-moon moons)]
+    (map assoc-dv-in-moon changes-per-moon)))
+
+;/////////////////////////////
+
+(defn create-moon
+  ([moon-id moon-str]
+    {:id moon-id
+     :pos (moon-pos-from-string moon-str)
+     :vel '(0 0 0)})
+  ([moon-id pos vel]
+   {:id moon-id
+    :pos pos
+    :vel vel}))
+
+(defn create-moons [str-list]
+  (map-indexed create-moon str-list))
+
+(defn apply-velocity [moon]
+  (let[new-vel (new-velocity moon)]
+    (create-moon
+     (:id moon)
+     (new-pos (:pos moon) new-vel)
+     new-vel)))
+
+(defn apply-time
+  ([moons steps]
+   (nth (iterate apply-time moons) steps))
+  ([moons]
+    (let[moons-with-dv (apply-gravity moons)]
+      (map apply-velocity moons-with-dv))))
+
+(defn energy [coords]
+  (reduce #(+ (Math/abs %1) (Math/abs %2)) coords))
+
+(defn potential-energy [moon]
+  (energy (:pos moon)))
+
+(defn kenetic-energy [moon]
+  (energy (:vel moon)))
+
+(defn moon-energy [moon]
+  (* (potential-energy moon) (kenetic-energy moon)))
+
+(defn total-energy [moons steps]
+  (reduce + (map moon-energy (apply-time moons steps))))
