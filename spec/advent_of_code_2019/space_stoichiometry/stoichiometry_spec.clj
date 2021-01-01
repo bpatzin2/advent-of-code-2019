5b2f4ec1e3daa6587c97f482abf713a369c308de
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2021-01-01 09:51:41 +0100

    apply-reaction works

--- spec/advent_of_code_2019/space_stoichiometry/stoichiometry_spec.clj
+++ spec/advent_of_code_2019/space_stoichiometry/stoichiometry_spec.clj
@@ -4,11 +4,59 @@
 
 (describe "parsing"
   (it "parses a reaction"
-      (should= {:in {"ORE" 9} :out {"A" 2}}
-               (parse-reaction "9 ORE => 2 A"))
-      (should= {:in {"A" 7 "E" 1} :out {"FUEL" 1}}
-               (parse-reaction "7 A, 1 E => 1 FUEL")))
+      (should=
+        {:in {"ORE" 9} :out {"A" 2}}
+        (parse-reaction "9 ORE => 2 A"))
+      (should=
+        {:in {"A" 7 "E" 1} :out {"FUEL" 1}}
+        (parse-reaction "7 A, 1 E => 1 FUEL")))
   (it "parses reactions"
-      (should= [{:in {"ORE" 10} :out {"A" 10}}
-                {:in {"ORE" 1} :out {"B" 1}}]
-               (parse-reactions "10 ORE => 10 A\n1 ORE => 1 B"))))
+      (should=
+        [{:in {"ORE" 10} :out {"A" 10}}
+         {:in {"ORE" 1} :out {"B" 1}}]
+        (parse-reactions "10 ORE => 10 A\n1 ORE => 1 B"))))
+
+(describe "requirement-satisfied?"
+  (let [ingredients {"ORE" 5}]
+    (it "requirement-satisfied?"
+        (should=
+          true
+          (requirement-satisfied? ingredients (first {"ORE" 5})))
+        (should=
+          false
+          (requirement-satisfied? ingredients (first {"ORE" 6})))
+        (should=
+          false
+          (requirement-satisfied? ingredients (first {"B" 1}))))))
+
+(describe "requirements-satisfied?"
+  (let [ingredients {"ORE" 5 "A" 1 "B" 1}]
+    (it "requirement-satisfied?"
+        (should=
+          true
+          (requirements-satisfied? ingredients {:in {"ORE" 5 "A" 1}}))
+        (should=
+          false
+          (requirements-satisfied? ingredients {:in {"ORE" 5 "A" 2}}))
+        (should=
+          false
+          (requirements-satisfied? ingredients {:in {"ORE" 5 "A" 1 "C" 1}})))))
+
+(describe "consume-all"
+  (let [ingredients {"ORE" 5 "A" 2 "B" 1}]
+    (it "consume-all"
+        (should=
+          {"ORE" 4}
+          (consume ingredients (first {"ORE" 1})))
+        (should=
+          {"ORE" 0 "A" 1 "B" 1}
+          (consume-all ingredients {:in {"ORE" 5 "A" 1}})))))
+
+(describe "apply-reaction"
+  (let [ingredients {"ORE" 5 "A" 2 "B" 1}
+        reaction {:in {"ORE" 5 "A" 1} :out {"C" 1}}
+        expected {"ORE" 0 "A" 1 "B" 1 "C" 1}]
+    (it "apply-reaction"
+        (should= expected (apply-reaction ingredients reaction)))))
+
+
--- src/advent_of_code_2019/space_stoichiometry/stoichiometry.clj
+++ src/advent_of_code_2019/space_stoichiometry/stoichiometry.clj
@@ -16,3 +16,47 @@
 
 (defn parse-reactions [reaction-str]
   (mapv parse-reaction (str/split reaction-str #"\n")))
+
+; Can you produce a fuel given this much ORE?
+; What's the cheapest way to produce this SET of CHEMs?
+; What are all the different assemblies of CHEMs that could
+; produce that set?
+; Can you DP b/c there'll be a lot of overlapping pieces
+;
+; What's the cheapest way to produce set X?
+; What are all of the different next-level components that go
+; into producing X? And what's the cheapest way to produce those?
+; End on ORE
+
+(defn requirement-satisfied? [ingredients reaction-requirement]
+ (let [required-chem (key reaction-requirement)
+       required-amount (val reaction-requirement)
+       ingredient-amount (get ingredients required-chem 0)]
+   (>= ingredient-amount required-amount)))
+
+
+(defn requirements-satisfied? [ingredients reaction]
+  (every? #(requirement-satisfied? ingredients %) (:in reaction)))
+
+(defn consume [ingredients reaction-requirement]
+  (let [required-chem (key reaction-requirement)
+        required-amount (val reaction-requirement)
+        ingredient-amount (get ingredients required-chem 0)]
+    {required-chem (- ingredient-amount required-amount)}))
+
+(defn consume-all [ingredients reaction]
+  (let [inputs (:in reaction)
+        new-ingredient-amounts (map #(consume ingredients %) inputs)]
+    (into ingredients new-ingredient-amounts)))
+
+(defn apply-reaction [ingredients reaction]
+  (into (consume-all ingredients reaction) (:out reaction)))
+
+(defn possible-next-states [ingredients, reactions]
+  (let [applicable-reactions (filter #(requirements-satisfied? ingredients %) reactions)]
+    (mapv #(apply-reaction ingredients %) applicable-reactions)))
+
+;(map #(apply-reaction ingredients %) applicable-reactions)
+
+
+
