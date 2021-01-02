2e0968e2c63c30619f36a5a402155f886b31c52f
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2021-01-02 18:54:06 +0100

    day14 pt1 works

--- src/advent_of_code_2019/space_stoichiometry/stoichiometry.clj
+++ src/advent_of_code_2019/space_stoichiometry/stoichiometry.clj
@@ -1,6 +1,5 @@
 (ns advent-of-code-2019.space-stoichiometry.stoichiometry
-  (:require [clojure.string :as str]
-            [clojure.set :as set-lib]))
+  (:require [clojure.string :as str]))
 
 (defn parse-chem-quantity [quantity-chem-str]
   (let [qaunt-chem (str/split quantity-chem-str #" ")
@@ -53,21 +52,73 @@
 (defn required-inputs [input reaction-count]
   (map (fn [[k v]] {k (* v reaction-count)}) input))
 
+(defn output-chem [reaction]
+  (first (keys (:out reaction))))
+
+(defn output-quantity [reaction]
+  (first (vals (:out reaction))))
+
+(defn all-output-chems [reactions]
+  (map output-chem reactions))
+
+(defn ingredient-chem [ingredient]
+  (first ingredient))
+
+(defn ingredient-quantity [ingredient]
+  (second ingredient))
+
 (defn decompose [ingredient reaction]
   (let [in (:in reaction)
-        reaction-chem (first (keys (:out reaction)))
-        reaction-quantity (first (vals (:out reaction)))
-        quantity-required (second ingredient)
+        reaction-chem (output-chem reaction)
+        reaction-quantity (output-quantity reaction)
+        quantity-required (ingredient-quantity ingredient)
         reaction-count (reaction-count quantity-required reaction-quantity)
         amount-produced (* reaction-count reaction-quantity)]
     {:result (into {} (required-inputs in reaction-count))
      :left-over {reaction-chem (- amount-produced quantity-required)}}))
 
 (defn find-and-decompose [ingredient reactions]
-  (let [reaction (first (filter #(= (first ingredient) (first (keys (:out %)))) reactions))]
+  (let [reaction (first (filter #(= (ingredient-chem ingredient) (output-chem %)) reactions))]
     (decompose ingredient reaction)))
 
-(defn decompose-all [ingredients reactions]
-  (let [decomposed (map #(find-and-decompose % reactions) ingredients)]
-    {:result (apply #(merge-with + %1 %2) (map :result decomposed))
-     :left-over (apply #(merge-with + %1 %2) (map :left-over decomposed))}))
+(defn remaining? [remaining-ingredients reaction]
+  (contains? (set remaining-ingredients) (output-chem reaction)))
+
+(defn not-still-input [ingredient remaining reactions]
+  (let [remaining-reactions (filter #(remaining? remaining %) reactions)
+        remaining-inputs (set (flatten (map keys (map :in remaining-reactions))))]
+    (not (contains? remaining-inputs ingredient))))
+
+(defn next-ingredient [remaining reactions]
+  (let [candidates (filter #(not-still-input % remaining reactions) remaining)]
+    (first candidates)))
+
+(defn topo-sort [reactions]
+  (loop [result []
+         remaining (all-output-chems reactions)]
+    (if (empty? remaining)
+      result
+      (let [next (next-ingredient remaining reactions)]
+        (recur (conj result next) (remove #(= next %) remaining))))))
+
+(defn find-ingredient [ingredients ingredient]
+  (first (filter #(= ingredient (ingredient-chem %)) ingredients)))
+
+(defn decompose-all2 [reactions]
+  (let [ingredient-order (topo-sort reactions)]
+    (loop [rest ingredient-order
+           result {"FUEL" 1}]
+      (if (empty? rest)
+        result
+        (let [next (find-ingredient result (first rest))
+              decomposed (find-and-decompose next reactions)
+              remove-decomposed (dissoc result (ingredient-chem next))
+              new-result (merge-with + remove-decomposed (:result decomposed))]
+          (recur (drop 1 rest) new-result))))))
+
+(defn min-ore-to-reach-fuel [reactions]
+  (get (decompose-all2 reactions) "ORE"))
+
+(defn parse-and-min-ore-to-reach-fuel [reactions-str]
+  (let [reactions (parse-reactions reactions-str)]
+    (min-ore-to-reach-fuel reactions)))
