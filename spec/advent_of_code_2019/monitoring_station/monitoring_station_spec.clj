1d18221867852a8e2e431491e46456b2cd39c44f
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2021-01-04 21:10:52 +0100

    reenable some of the pt2 tests

--- spec/advent_of_code_2019/monitoring_station/monitoring_station_spec.clj
+++ spec/advent_of_code_2019/monitoring_station/monitoring_station_spec.clj
@@ -4,71 +4,70 @@
             [clojure.string :as str]
             [advent-of-code-2019.input-handling :as input]))
 
-;(def simple-grid
-;  [[\.\.\#]
-;   [\.\#\.]
-;   [\#\.\#]])
-;
-;(def complex-grid
-;  [[\.\.\#\#]
-;   [\.\#\.\.]
-;   [\#\.\#\.]])
-;
-;(describe
-;  "get-visible-asteroid-coords"
-;  (it "works for simple input"
-;      (let [visible-asteroids (visible-asteroid-coords
-;                               (create-coord 0 0) simple-grid)]
-;        (should= [{:x 2, :y 0} {:x 1, :y 1} {:x 0, :y 2}]  (vec visible-asteroids))))
-;  (it "works for complex input"
-;      (let [visible-asteroids (visible-asteroid-coords
-;                                (create-coord 3 2) complex-grid)]
-;        (should= #{{:x 2, :y 2} {:x 1, :y 1} {:x 2, :y 0} {:x 3, :y 0}}  (set visible-asteroids)))))
-;
-;(def grid-with-clear-best
-;  [[\.\.\.]
-;   [\#\#\#]])
-;
-;(describe
-;  "best-location-w-count"
-;  (it "works for simple input"
-;      (should= {:coord {:x 1 :y 1} :count 2}
-;               (best-location-w-count grid-with-clear-best))))
-;
-;(describe
-;  "best-location-w-count"
-;  (it "works for test file input"
-;      (should= {:coord {:x 11, :y 13}, :count 210}
-;               (best-location-w-count (input/day10-grid "input/day10TestLarge.txt")))))
-;
-;(describe
-;  "count-visible-from-best-loc"
-;  (it "works for test file input"
-;      (should= {:x 11, :y 13}
-;               (best-location (input/day10-grid "input/day10TestLarge.txt")))))
-;
-;(def grid-with-laser
-;  [[\.\.\#\#]
-;   [\.\X\.\.]
-;   [\#\.\#\.]])
-;
-;(describe
-;  "vaporize-asteroids"
-;  (it "works for simple input"
-;      (should= {:x 0 :y 2}
-;               (vaporize-asteroids grid-with-laser 4 (create-coord 1 1)))))
+(def simple-grid
+  [[\.\.\#]
+   [\.\#\.]
+   [\#\.\#]])
 
-(def test-grid
-  [[\.\.\.\.]
-   [\.\.\#\.]
-   [\X\.\.\#]])
+(def complex-grid
+  [[\.\.\#\#]
+   [\.\#\.\.]
+   [\#\.\#\.]])
 
 (describe
-  "vaporize-asteroids"
+  "get-visible-asteroid-coords"
   (it "works for simple input"
-      (should= {:x 2 :y 1}
-               (vaporize-asteroids test-grid 1 (create-coord 0 2)))))
+      (let [visible-asteroids (visible-asteroid-coords
+                               (create-coord 0 0) simple-grid)]
+        (should= [{:x 2, :y 0} {:x 1, :y 1} {:x 0, :y 2}]  (vec visible-asteroids))))
+  (it "works for complex input"
+      (let [visible-asteroids (visible-asteroid-coords
+                                (create-coord 3 2) complex-grid)]
+        (should= #{{:x 2, :y 2} {:x 1, :y 1} {:x 2, :y 0} {:x 3, :y 0}}  (set visible-asteroids)))))
 
+(def grid-with-clear-best
+  [[\.\.\.]
+   [\#\#\#]])
+
+(describe
+  "best-location-w-count"
+  (it "works for simple input"
+      (should= {:coord {:x 1 :y 1} :count 2}
+               (best-location-w-count grid-with-clear-best))))
+
+(describe
+  "best-location-w-count"
+  (it "works for test file input"
+      (should= {:coord {:x 11, :y 13}, :count 210}
+               (best-location-w-count (input/day10-grid "input/day10TestLarge.txt")))))
+
+(describe
+  "count-visible-from-best-loc"
+  (it "works for test file input"
+      (should= {:x 11, :y 13}
+               (best-location (input/day10-grid "input/day10TestLarge.txt")))))
+
+(describe "vaporize-asteroids"
+  (it "works for simple input"
+      (let [grid-with-laser [[\.\.\#\#]
+                             [\.\X\.\.]
+                             [\#\.\#\.]]]
+        (should=
+          {:x 0 :y 2}
+          (vaporize-asteroids grid-with-laser 4 (create-coord 1 1))))))
+
+  ;TODO: This is the failing test case to work on
+  ;(it "works for grid input with asteroid not aligned with edge"
+  ;  (let [laser (create-coord 0 2)
+  ;        grid [[\.\.\.\.]
+  ;              [\.\.\#\.]
+  ;              [\X\.\.\#]]]
+  ;    (should=
+  ;      {:x 2 :y 1}
+  ;      (vaporize-asteroids grid 1 laser)))))
+
+
+;TODO: Example test case
 ;.#....###24...#..
 ;##...##.13#67..9#
 ;##...#...5.8####.
