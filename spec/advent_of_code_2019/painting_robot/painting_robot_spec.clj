4fbf0aaa51649c0c9c5b618b45b64218e93414a8
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2021-01-10 15:43:55 +0100

    fix input passing

--- spec/advent_of_code_2019/painting_robot/painting_robot_spec.clj
+++ spec/advent_of_code_2019/painting_robot/painting_robot_spec.clj
@@ -159,7 +159,7 @@
   ;(println (:dir state) (:pos state) input)
   ;(if (= 10 i) (throw (Exception. "my exception message")) nil)
   (let [robot-exe-state (:robot-exe-state state)
-        next-exe-state (intcode/execute-segment robot-exe-state [input] false)
+        next-exe-state (intcode/execute-segment robot-exe-state input false)
         status (:status next-exe-state)
         output (:output next-exe-state)
         robot-state (assoc next-exe-state :output [])]
