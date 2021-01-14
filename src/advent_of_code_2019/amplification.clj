fdf2b6ed3a90eebb457b32b3f59abf2505312100
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2021-01-14 19:30:33 +0100

    delete unused code in amp

--- src/advent_of_code_2019/amplification.clj
+++ src/advent_of_code_2019/amplification.clj
@@ -3,32 +3,6 @@
   (:require [advent-of-code-2019.intcode.intcode :as intcode]
             [clojure.math.combinatorics :as combo]))
 
-(defn all-test-inputs []
-  (combo/cartesian-product (range 0 100) (range 0 100)))
-
-(defn create-test-program [program, test-input-pair]
-  (assoc program 1 (first test-input-pair) 2 (second test-input-pair)))
-
-(defn all-test-programs [program]
-  (map #(create-test-program program %) (all-test-inputs)))
-
-(defn is-desired-program [program desired-output]
-  (= desired-output (first program)))
-
-(defn filter-desired-output [desired-output programs]
-  (filter #(is-desired-program % desired-output) programs))
-
-(defn pluck-input [program]
-  [(nth program 1) (nth program 2)])
-
-(defn find-inputs [desired-output program]
-  (->> program
-       (all-test-programs)
-       (map intcode/execute)
-       (filter-desired-output desired-output)
-       (first)
-       (pluck-input)))
-
 (defn run-in-series [program phase-settings]
   (loop [rem-phase-settings phase-settings
          input-signal 0]
@@ -44,12 +18,12 @@
   (let [possible-settings (combo/permutations phase-settings)]
     (loop [rem-possible-settings possible-settings
            largest-output 0]
-    (if (empty? rem-possible-settings)
+      (if (empty? rem-possible-settings)
         largest-output
         (recur 
          (rest rem-possible-settings) 
          (let [output (run-in-series program (first rem-possible-settings))]
-               (max output largest-output)))))))
+           (max output largest-output)))))))
 
 (defn merge-amps [new-amp amps index]
   (assoc amps index new-amp))
@@ -63,8 +37,8 @@
          program (get amp :program)]
     (if
      (empty? rem-inputs)
-      (assoc updated-amp :input [])
-      (let [input (first rem-inputs)
+     (assoc updated-amp :input [])
+     (let [input (first rem-inputs)
             new-state (intcode/execute-segment program addr input outputs relative-base false)
             new-amp (merge updated-amp new-state)
             next-addr (:addr new-amp)
@@ -74,23 +48,23 @@
         (recur (rest rem-inputs) next-addr new-outputs new-relative-base new-amp new-prog)))))
 
 (defn run-in-loop [program phase-settings]
-(loop [amps   
-       [{:program program :name "a" :input [(phase-settings 0)]}
-        {:program program :name "b" :input [(phase-settings 1)]}
-        {:program program :name "c" :input [(phase-settings 2)]}
-        {:program program :name "d" :input [(phase-settings 3)]}
-        {:program program :name "e" :input [(phase-settings 4)]}]
-       curr-index 0
-       prev-output 0]
-  (if
-   (and (= 0 curr-index) (= :stopped (get (amps 4) :status)))
-    (last (get (amps 4) :output))
-    (let [amp (amps curr-index)
-          updated-amp (process-amp amp prev-output)
-          updated-amps (merge-amps updated-amp amps curr-index)
-          outputs (:output (updated-amps curr-index))
-          next-index (rem (inc curr-index) 5)]
-      (recur updated-amps next-index (last outputs))))))
+  (loop [amps
+         [{:program program :name "a" :input [(phase-settings 0)]}
+          {:program program :name "b" :input [(phase-settings 1)]}
+          {:program program :name "c" :input [(phase-settings 2)]}
+          {:program program :name "d" :input [(phase-settings 3)]}
+          {:program program :name "e" :input [(phase-settings 4)]}]
+         curr-index 0
+         prev-output 0]
+    (if
+     (and (= 0 curr-index) (= :stopped (get (amps 4) :status)))
+     (last (get (amps 4) :output))
+     (let [amp (amps curr-index)
+           updated-amp (process-amp amp prev-output)
+           updated-amps (merge-amps updated-amp amps curr-index)
+           outputs (:output (updated-amps curr-index))
+           next-index (rem (inc curr-index) 5)]
+       (recur updated-amps next-index (last outputs))))))
 
 (defn find-largest-loop-output [program phase-settings]
   (let [possible-settings (combo/permutations phase-settings)]
